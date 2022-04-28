aa9cf5c6619009c9051fc54c35130317b68a8f87
Isa Pérez <isperez@aidacanarias.com>
2022-04-28 10:53:56 +0200

    🛠 Added Solution and Test project

---
+++ CitiesSearchProviderSpecs/CitiesSearchProviderShould.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace CitiesSearchProviderSpecs
+{
+    public class CitiesSearchProviderShould
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
