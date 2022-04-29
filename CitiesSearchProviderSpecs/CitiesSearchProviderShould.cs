f44b9e9c5b369e86522cd735da361a1e4e26b75f
Isa Pérez <isperez@aidacanarias.com>
2022-04-29 10:27:07 +0200

    🧪 New test return_Valencia_and_Vancouver_when_input_text_is_Va

--- CitiesSearchProviderSpecs/CitiesSearchProviderShould.cs
+++ CitiesSearchProviderSpecs/CitiesSearchProviderShould.cs
@@ -16,6 +16,18 @@ namespace CitiesSearchProviderSpecs
 
             Assert.IsTrue(!results.Any());
         }
+
+        [Test]
+        public void return_Valencia_and_Vancouver_when_input_text_is_Va()
+        {
+            const string inputText = "Va";
+            var citiesSearchProvider = new CitiesSearchProvider();
+
+            var results = citiesSearchProvider.Search(inputText);
+
+            Assert.IsTrue(results.Contains("Valencia"));
+            Assert.IsTrue(results.Contains("Vancouver"));
+        }
     }
 
     public class CitiesSearchProvider
